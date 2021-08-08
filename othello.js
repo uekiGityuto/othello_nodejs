9c9c27f2a5f3145831257f0a783b5d532d38e78a
kakudaisuke <dk.15step@gmail.com>
2021-08-08 14:18:14 +0900

    now:を追加

--- othello.js
+++ othello.js
@@ -46,8 +46,7 @@ const MAX = 60, MIN = 0;
 let value = 30, key;
 console.log(`\n\n${(new Array(20)).join(' ')}[Y] <- -> [X]  決定: [q]\n`);
 while (true) {
-  console.log('\x1B[1A\x1B[K|' +
-    (new Array(value + 1)).join('-') + 'O' + (new Array(MAX - value + 1)).join('-') + '| ' + value);
+  console.log('\x1B[1A\x1B[K|' + (new Array(value + 1)).join('-') + 'O' + (new Array(MAX - value + 1)).join('-') + '| ' + 'now:' + value);
   key = readlineSync.keyIn('',
     {hideEchoBack: true, mask: '', limit: 'yxpq'});
   if (key === 'y') { if (value > MIN) { value-=2; } }
