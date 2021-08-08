fe2bff55d5be4af3f320ea513b3236dbe8c1bd36
kakudaisuke <dk.15step@gmail.com>
2021-08-08 14:10:27 +0900

    横に移動できた

--- othello.js
+++ othello.js
@@ -1,7 +1,8 @@
+const readlineSync = require('readline-sync');
+
 console.log("スペースもしくはEnterで石置けます")
 console.log("やめたい時はqを押す")
 console.log("白の番")
-
 
 class Board {
   drawBoard() {
@@ -36,7 +37,22 @@ class Board {
 //   return board;
 // }
 
-
-const board = new Board
+// const board = new Board
 // drawBoard_tomoya()
-console.log(board.drawBoard());
+// console.log(board.drawBoard());
+
+
+const MAX = 60, MIN = 0;
+let value = 30, key;
+console.log(`\n\n${(new Array(20)).join(' ')}[Y] <- -> [X]  決定: [q]\n`);
+while (true) {
+  console.log('\x1B[1A\x1B[K|' +
+    (new Array(value + 1)).join('-') + 'O' + (new Array(MAX - value + 1)).join('-') + '| ' + value);
+  key = readlineSync.keyIn('',
+    {hideEchoBack: true, mask: '', limit: 'yxpq'});
+  if (key === 'y') { if (value > MIN) { value-=2; } }
+  else if (key === 'x') { if (value < MAX) { value+=2; } }
+  else if (key === 'p') { value = 'O' }
+  else if (key === 'q') { break; }
+}
+console.log(`\nあなたが決めた値: ${value}`);
